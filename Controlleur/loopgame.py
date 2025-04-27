1caec55d24dbaf39ad13bde1b9fcf588ce3a31e9
edoua <jouhriedouard@yahoo.com>
2025-04-27 16:41:11 +0200

    voila

--- Controlleur/loopgame.py
+++ Controlleur/loopgame.py
@@ -7,41 +7,32 @@ class Game(QWidget):
         super().__init__()
         self.setWindowTitle("Mon Jeu")
         board = Plateau()
-        tile1 = Tuile('images/img1.jpg')
-        tile2 = Tuile('images/img1.jpg')
-        tile3 = Tuile('images/img1.jpg')
-        tile4 = Tuile('images/img1.jpg')
+        self.liste_images = [
+    "image/anim.jpeg",
+    "image/arrie.jpeg",
+    "image/arrire.jpeg",
+    "image/aura.jpg",
+    "image/colored_pencil_sketch.jpg",
+    "image/cosmic.jpg",
+    "image/demon.png",
+    "image/enchanted.jpeg",
+    "image/img1.jpg",
+    "image/machien.jpg",
+    "image/p.jpg",
+    "image/papier.png",
+    "image/pencil_sketch.jpg",
+    "image/roule dessus.png",
+    "image/s.png",
+    "image/temple.jpg"
+]
 
-        tile5 = Tuile('images/img1.jpg')
-        tile6 = Tuile('images/img1.jpg')
-        tile7 = Tuile('images/img1.jpg')
-        tile8 = Tuile('images/img1.jpg')
+        index = 0
+        for x in range(4):
+            for y in range(4):
+                if index < len(self.liste_images):
+                    icone = Tuile(self.liste_images[index])
+                board.ajouter_tuile(icone, x, y)
+                index += 1
 
-        tile9 = Tuile('images/img1.jpg')
-        tile10 = Tuile('images/img1.jpg')
-        tile11 = Tuile('images/img1.jpg')
-        tile12 = Tuile('images/img1.jpg')
-
-        tile13 = Tuile('images/img1.jpg')
-        tile14 = Tuile('images/img1.jpg')
-        tile15 = Tuile('images/img1.jpg')
-        tile16 = Tuile('images/img1.jpg')
-
-        board.ajouter_tuile(tile1,0,0)
-        board.ajouter_tuile(tile2,0,1)
-        board.ajouter_tuile(tile3, 0, 2)
-        board.ajouter_tuile(tile4, 0, 3)
-        board.ajouter_tuile(tile5, 1, 0)
-        board.ajouter_tuile(tile6, 1, 1)
-        board.ajouter_tuile(tile7, 1, 2)
-        board.ajouter_tuile(tile8, 1, 3)
-        board.ajouter_tuile(tile9, 2, 0)
-        board.ajouter_tuile(tile10, 2, 1)
-        board.ajouter_tuile(tile11, 2, 2)
-        board.ajouter_tuile(tile12, 2, 3)
-        board.ajouter_tuile(tile13, 3, 0)
-        board.ajouter_tuile(tile14, 3, 1)
-        board.ajouter_tuile(tile15, 3, 2)
-        board.ajouter_tuile(tile16, 3, 3)
 
         self.setLayout(board.layout)
